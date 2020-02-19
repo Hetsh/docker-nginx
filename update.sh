3d45521408d48b5d440f15f320837087c32ed6e0
Hetsh <git@hetsh.de>
2020-02-19 17:03:41 +0100

    Use simple update functions

--- update.sh
+++ update.sh
@@ -17,37 +17,13 @@ assert_dependency "jq"
 assert_dependency "curl"
 
 # Current version of docker image
-CURRENT_VERSION=$(git describe --tags --abbrev=0)
-register_current_version "$CURRENT_VERSION"
+register_current_version
 
 # Alpine Linux
-IMAGE_PKG="alpine"
-IMAGE_NAME="Alpine"
-IMAGE_REGEX="(\d+\.)+\d+"
-IMAGE_TAGS=$(curl -L -s "https://registry.hub.docker.com/v2/repositories/library/$IMAGE_PKG/tags" | jq '."results"[]["name"]' | grep -P -o "$IMAGE_REGEX")
-IMAGE_VERSION=$(echo "$IMAGE_TAGS" | sort --version-sort | tail -n 1)
-CURRENT_IMAGE_VERSION=$(cat "Dockerfile" | grep -P -o "FROM $IMAGE_PKG:\K$IMAGE_REGEX")
-if [ "$CURRENT_IMAGE_VERSION" != "$IMAGE_VERSION" ]; then
-	echo "$IMAGE_NAME $IMAGE_VERSION available!"
-	update_release
-fi
+update_image "alpine" "Alpine" "x86_64" "(\d+\.)+\d+"
 
 # NGINX
-NGINX_PKG="nginx"
-NGINX_NAME="NGINX"
-NGINX_REGEX="(\d+\.)+\d+-r\d+"
-NGINX_VERSION=$(curl -L -s "https://pkgs.alpinelinux.org/package/v${IMAGE_VERSION%.*}/main/x86_64/$NGINX_PKG" | grep -m 1 -P -o "$NGINX_REGEX")
-CURRENT_NGINX_VERSION=$(cat Dockerfile | grep -P -o "$NGINX_PKG=\K$NGINX_REGEX")
-if [ "$CURRENT_NGINX_VERSION" != "$NGINX_VERSION" ]; then
-	echo "$NGINX_NAME $NGINX_VERSION available!"
-
-	# Dont update version if only release counter changed
-	if [ "${CURRENT_NGINX_VERSION%-*}" != "${NGINX_VERSION%-*}" ]; then
-		update_version "${NGINX_VERSION%-*}"
-	else
-		update_release
-	fi
-fi
+update_alpine_pkg "nginx" "NGINX" "true" "main" "(\d+\.)+\d+-r\d+"
 
 if ! updates_available; then
 	echo "No updates available."
@@ -55,17 +31,9 @@ if ! updates_available; then
 fi
 
 if [ "${1+}" = "--noconfirm" ] || confirm_action "Save changes?"; then
-	if [ "$CURRENT_IMAGE_VERSION" != "$IMAGE_VERSION" ]; then
-		sed -i "s|FROM $IMAGE_PKG:$CURRENT_IMAGE_VERSION|FROM $IMAGE_PKG:$IMAGE_VERSION|" Dockerfile
-		CHANGELOG+="$IMAGE_NAME $CURRENT_IMAGE_VERSION -> $IMAGE_VERSION, "
-	fi
-	if [ "$CURRENT_NGINX_VERSION" != "$NGINX_VERSION" ]; then
-		sed -i "s|$NGINX_PKG=$CURRENT_NGINX_VERSION|$NGINX_PKG=$NGINX_VERSION|" Dockerfile
-		CHANGELOG+="$NGINX_NAME $CURRENT_NGINX_VERSION -> $NGINX_VERSION, "
-	fi
-	CHANGELOG="${CHANGELOG%,*}"
+	save_changes
 
 	if [ "${1+}" = "--noconfirm" ] || confirm_action "Commit changes?"; then
-		commit_changes "$CHANGELOG"
+		commit_changes
 	fi
 fi
