93911560b2159f84ca390576ed7bcd64bac97c38
Hetsh <git@hetsh.de>
2020-03-14 20:41:50 +0100

    Fix typos, added additional SRV dir

--- build.sh
+++ build.sh
@@ -11,11 +11,11 @@ cd "$CWD"
 # Load helpful functions
 source libs/common.sh
 
-# Check acces do docker daemon
+# Check access to docker daemon
 assert_dependency "docker"
 if ! docker version &> /dev/null; then
-    echo "Docker daemon is not running or you have unsufficient permissions!"
-    exit -1
+	echo "Docker daemon is not running or you have unsufficient permissions!"
+	exit -1
 fi
 
 # Build the image
@@ -24,8 +24,8 @@ docker build --tag "$APP_NAME" .
 
 if confirm_action "Test image?"; then
 	# Set up temporary directory
-	TMP_DIR=$(mktemp -d "/tmp/$APP_NAME-XXXXXXXXXX")
-	add_cleanup "rm -rf $TMP_DIR"
+	TMP_HOSTS_DIR=$(mktemp -d "/tmp/$APP_NAME-HOSTS-XXXXXXXXXX")
+	add_cleanup "rm -rf $TMP_HOSTS_DIR"
 	echo "server {
 	listen 80 default_server;
 	listen [::]:80 default_server;
@@ -38,7 +38,10 @@ if confirm_action "Test image?"; then
 	location / {
 		try_files \$uri \$uri/ =404;
 	}
-}" > "$TMP_DIR/test.conf"
+}" > "$TMP_HOSTS_DIR/test.conf"
+
+	TMP_SRV_DIR=$(mktemp -d "/tmp/$APP_NAME-SRV-XXXXXXXXXX")
+	add_cleanup "rm -rf $TMP_SRV_DIR"
 	echo "<html>
 	<head>
 		<title>Test</title>
@@ -46,12 +49,12 @@ if confirm_action "Test image?"; then
 	<body>
 		<h1>Success!</h1>
 	</body>
-</html>" > "$TMP_DIR/index.html"
+</html>" > "$TMP_SRV_DIR/index.html"
 
 	# Apply permissions, UID & GID matches process user
 	extract_var APP_UID "./Dockerfile" "\K\d+"
 	extract_var APP_GID "./Dockerfile" "\K\d+"
-	chown -R "$APP_UID":"$APP_GID" "$TMP_DIR"
+	chown -R "$APP_UID":"$APP_GID" "$TMP_HOSTS_DIR" "$TMP_SRV_DIR"
 
 	# Start the test
 	extract_var HOSTS_DIR "./Dockerfile" "\"\K[^\"]+"
@@ -61,8 +64,9 @@ if confirm_action "Test image?"; then
 	--interactive \
 	--publish 80:80/tcp \
 	--publish 443:443/tcp \
-	--mount type=bind,source="$TMP_DIR",target="$HOSTS_DIR" \
-	--mount type=bind,source="$TMP_DIR/index.html",target="$SRV_DIR/index.html" \
+	--mount type=bind,source="$TMP_HOSTS_DIR",target="$HOSTS_DIR" \
+	--mount type=bind,source="$TMP_SRV_DIR",target="$SRV_DIR" \
+	--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
 	--name "$APP_NAME" \
 	"$APP_NAME"
 fi
