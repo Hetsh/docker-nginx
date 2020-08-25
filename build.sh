6adb3aeb000b7dd011c44c79853bf1cdab26ae85
Hetsh <git@hetsh.de>
2020-08-25 13:00:01 +0200

    Added upload option

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ cd "$CWD"
 
 # Load helpful functions
 source libs/common.sh
+source libs/docker.sh
 
 # Check access to docker daemon
 assert_dependency "docker"
@@ -20,15 +21,16 @@ fi
 
 # Build the image
 APP_NAME="nginx"
-APP_TAG="hetsh/$APP_NAME"
-docker build --tag "$APP_TAG" --tag "$APP_TAG:$(git describe --tags --abbrev=0)" .
+IMG_NAME="hetsh/$APP_NAME"
+docker build --tag "$IMG_NAME" --tag "$IMG_NAME:$(git describe --tags --abbrev=0)" .
 
 # Start the test
-if [ "${1-}" = "--test" ]; then
-	# Set up temporary directory
-	TMP_DIR=$(mktemp -d "/tmp/$APP_NAME-XXXXXXXXXX")
-	add_cleanup "rm -rf $TMP_DIR"
-	echo "server {
+case "${1-}" in
+	"--test")
+		# Set up temporary directory
+		TMP_DIR=$(mktemp -d "/tmp/$APP_NAME-XXXXXXXXXX")
+		add_cleanup "rm -rf $TMP_DIR"
+		echo "server {
 	listen 80 default_server;
 	listen [::]:80 default_server;
 	server_name _;
@@ -40,7 +42,7 @@ if [ "${1-}" = "--test" ]; then
 		try_files \$uri \$uri/ =404;
 	}
 }" > "$TMP_DIR/test.conf"
-	echo "<html>
+		echo "<html>
 	<head>
 		<title>Test</title>
 	</head>
@@ -48,28 +50,34 @@ if [ "${1-}" = "--test" ]; then
 		<h1>Success!</h1>
 	</body>
 </html>" > "$TMP_DIR/index.html"
-	mkdir "$TMP_DIR/logs"
+		mkdir "$TMP_DIR/logs"
 
 
-	# Apply permissions, UID & GID matches process user
-	extract_var APP_UID "./Dockerfile" "\K\d+"
-	extract_var APP_GID "./Dockerfile" "\K\d+"
-	chown -R "$APP_UID":"$APP_GID" "$TMP_DIR"
+		# Apply permissions, UID & GID matches process user
+		extract_var APP_UID "./Dockerfile" "\K\d+"
+		extract_var APP_GID "./Dockerfile" "\K\d+"
+		chown -R "$APP_UID":"$APP_GID" "$TMP_DIR"
 
-	# Start the test
-	extract_var HOST_DIR "./Dockerfile" "\"\K[^\"]+"
-	extract_var SRV_DIR "./Dockerfile" "\"\K[^\"]+"
-	extract_var LOG_DIR "./Dockerfile" "\"\K[^\"]+"
-	docker run \
-	--rm \
-	--tty \
-	--interactive \
-	--publish 80:80/tcp \
-	--publish 443:443/tcp \
-	--mount type=bind,source="$TMP_DIR/test.conf",target="$HOST_DIR/test.conf" \
-	--mount type=bind,source="$TMP_DIR",target="$SRV_DIR" \
-	--mount type=bind,source="$TMP_DIR/logs",target="$LOG_DIR" \
-	--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
-	--name "$APP_NAME" \
-	"$APP_NAME"
-fi
+		# Start the test
+		extract_var HOST_DIR "./Dockerfile" "\"\K[^\"]+"
+		extract_var SRV_DIR "./Dockerfile" "\"\K[^\"]+"
+		extract_var LOG_DIR "./Dockerfile" "\"\K[^\"]+"
+		docker run \
+		--rm \
+		--tty \
+		--interactive \
+		--publish 80:80/tcp \
+		--publish 443:443/tcp \
+		--mount type=bind,source="$TMP_DIR/test.conf",target="$HOST_DIR/test.conf" \
+		--mount type=bind,source="$TMP_DIR",target="$SRV_DIR" \
+		--mount type=bind,source="$TMP_DIR/logs",target="$LOG_DIR" \
+		--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
+		--name "$APP_NAME" \
+		"$IMG_NAME"
+	;;
+	"--upload")
+		if ! tag_exists "$IMG_NAME"; then
+			docker push "$IMG_NAME"
+		fi
+	;;
+esac
