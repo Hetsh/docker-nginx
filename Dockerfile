c6e04be294b603ac77e43a29fc3ce9e8696ab83f
Hetsh <git@hetsh.de>
2020-11-16 11:57:40 +0100

    Keep default host, remove non default test options

--- Dockerfile
+++ Dockerfile
@@ -15,20 +15,20 @@ RUN sed -i "/:$APP_UID/d" /etc/passwd && \
     sed -i "s|$OLD_GROUP:x:[0-9]\+:$OLD_USER|$APP_GROUP:x:$APP_GID:|" /etc/group
 
 # Configuration
+ARG SRV_DIR="/srv"
+ARG HOST_DIR="/etc/nginx/conf.d"
 ARG NGINX_CONF="/etc/nginx/nginx.conf"
 RUN sed -i "s|^user $OLD_USER|user $APP_USER|" "$NGINX_CONF" && \
-    sed -i "s|^group $OLD_GROUP|group $APP_GROUP|" "$NGINX_CONF"
+    sed -i "s|^group $OLD_GROUP|group $APP_GROUP|" "$NGINX_CONF" && \
+    chown -R "$APP_USER":"$APP_GROUP" "$SRV_DIR" "$HOST_DIR"
 
 # Volumes
 ARG CERT_DIR="/etc/certs"
 ARG RUN_DIR="/run/nginx"
-ARG SRV_DIR="/srv"
 ARG LOG_DIR="/var/log/nginx"
-ARG HOST_DIR="/etc/nginx/conf.d"
 RUN mkdir "$CERT_DIR" "$RUN_DIR" && \
-    rm "$HOST_DIR/default.conf" && \
-    chown -R "$APP_USER":"$APP_GROUP" "$CERT_DIR" "$RUN_DIR" "$SRV_DIR" "$LOG_DIR" "$HOST_DIR"
-VOLUME ["$CERT_DIR", "$RUN_DIR", "$SRV_DIR", "$LOG_DIR", "$HOST_DIR"]
+    chown -R "$APP_USER":"$APP_GROUP" "$CERT_DIR" "$RUN_DIR" "$LOG_DIR"
+VOLUME ["$CERT_DIR", "$RUN_DIR", "$LOG_DIR", "$SRV_DIR", "$HOST_DIR"]
 
 #      HTTP   HTTPS
 EXPOSE 80/tcp 443/tcp
--- build.sh
+++ build.sh
@@ -27,50 +27,12 @@ docker build --tag "$IMG_NAME:latest" --tag "$IMG_NAME:$_NEXT_VERSION" .
 case "${1-}" in
 	# Test with default configuration
 	"--test")
-		# Set up temporary directory
-		TMP_DIR=$(mktemp -d "/tmp/$APP_NAME-XXXXXXXXXX")
-		add_cleanup "rm -rf $TMP_DIR"
-		echo "server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-	server_name _;
-
-	root /srv;
-	index index.html;
-
-	location / {
-		try_files \$uri \$uri/ =404;
-	}
-}" > "$TMP_DIR/test.conf"
-		echo "<html>
-	<head>
-		<title>Test</title>
-	</head>
-	<body>
-		<h1>Success!</h1>
-	</body>
-</html>" > "$TMP_DIR/index.html"
-		mkdir "$TMP_DIR/logs"
-
-
-		# Apply permissions, UID & GID matches process user
-		extract_var APP_UID "./Dockerfile" "\K\d+"
-		extract_var APP_GID "./Dockerfile" "\K\d+"
-		chown -R "$APP_UID":"$APP_GID" "$TMP_DIR"
-
-		# Start the test
-		extract_var HOST_DIR "./Dockerfile" "\"\K[^\"]+"
-		extract_var SRV_DIR "./Dockerfile" "\"\K[^\"]+"
-		extract_var LOG_DIR "./Dockerfile" "\"\K[^\"]+"
 		docker run \
 		--rm \
 		--tty \
 		--interactive \
 		--publish 80:80/tcp \
 		--publish 443:443/tcp \
-		--mount type=bind,source="$TMP_DIR/test.conf",target="$HOST_DIR/test.conf" \
-		--mount type=bind,source="$TMP_DIR",target="$SRV_DIR" \
-		--mount type=bind,source="$TMP_DIR/logs",target="$LOG_DIR" \
 		--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
 		--name "$APP_NAME" \
 		"$IMG_NAME"
